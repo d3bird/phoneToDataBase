2d7973dd48af4597f341625c87ac4ca5b7e107c1
Scott Crawford <dogbird9@gmail.com>
2017-04-29 11:05:59 -0400

    cammra function has been added to the chat

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,17 +3,17 @@ import { NavController } from 'ionic-angular';
 import { Message } from '../../models/message'
 import { MessageService } from '../../providers/message-service'
 import * as moment from 'moment';
-
+import { Camera, CameraOptions } from '@ionic-native/camera';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
-  providers: [MessageService]
+  providers: [MessageService,Camera]
 })
 export class HomePage {
   public USER_NAME_CONSTANT = 'Scott Crawford';
   public currentMessage: string;
 
-  constructor(public navCtrl: NavController, private messageService: MessageService) {
+  constructor(public navCtrl: NavController, private messageService: MessageService,private camera: Camera) {
   }
 
   public formatDateTo_hhmm(dateProvidedAsString: string) {
@@ -25,10 +25,33 @@ export class HomePage {
     this.currentMessage = "";
   }
 
+
+  public getPhoto(){
+
+       const options: CameraOptions = {
+        destinationType: this.camera.DestinationType.DATA_URL,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE,
+        targetWidth: 600,
+        targetHeight: 400
+      }
+
+    this.camera.getPicture(options).then((imageData) => {
+ // imageData is either a base64 encoded string or a file URI
+ // If it's base64:
+ let base64Image = 'data:image/jpeg;base64,' + imageData.replace(/[\n\r]/g, '');
+ this.buildAndSendMessage(base64Image);
+}, (err) => {
+ this.buildAndSendMessage("camera not found");
+   this.currentMessage = "";
+});
+  }
+
+
   private buildAndSendMessage(message: string) {
     var newMessage = new Message();
     newMessage.userName = this.USER_NAME_CONSTANT;
-    newMessage.messageContent = this.currentMessage;
+    newMessage.messageContent = message;
     this.messageService.addMessage(newMessage);
   }
 }
