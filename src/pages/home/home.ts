91e701584f0da1d035547e30633b3fe4d06ae695
Scott Crawford <dogbird9@gmail.com>
2017-04-29 12:25:55 -0400

    it now displays the images in the chat

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,16 +4,19 @@ import { Message } from '../../models/message'
 import { MessageService } from '../../providers/message-service'
 import * as moment from 'moment';
 import { Camera, CameraOptions } from '@ionic-native/camera';
+
+export const imageContentPrefix = 'data:image/jpeg;base64,';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
-  providers: [MessageService,Camera]
+  providers: [MessageService, Camera]
 })
 export class HomePage {
   public USER_NAME_CONSTANT = 'Scott Crawford';
   public currentMessage: string;
 
-  constructor(public navCtrl: NavController, private messageService: MessageService,private camera: Camera) {
+  constructor(public navCtrl: NavController, private messageService: MessageService, private camera: Camera) {
   }
 
   public formatDateTo_hhmm(dateProvidedAsString: string) {
@@ -25,26 +28,31 @@ export class HomePage {
     this.currentMessage = "";
   }
 
+   public doesThisMessageContainAnImage(message: Message) {
+    return message.messageContent.indexOf(imageContentPrefix) !== -1;
+  }
+public getImageFromMessageContent(message: Message) {
+    return this.doesThisMessageContainAnImage(message) ? message.messageContent : "";
+  }
+  public getPhoto() {
 
-  public getPhoto(){
-
-       const options: CameraOptions = {
-        destinationType: this.camera.DestinationType.DATA_URL,
-        encodingType: this.camera.EncodingType.JPEG,
-        mediaType: this.camera.MediaType.PICTURE,
-        targetWidth: 600,
-        targetHeight: 400
-      }
+    const options: CameraOptions = {
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      targetWidth: 600,
+      targetHeight: 400
+    }
 
     this.camera.getPicture(options).then((imageData) => {
- // imageData is either a base64 encoded string or a file URI
- // If it's base64:
- let base64Image = 'data:image/jpeg;base64,' + imageData.replace(/[\n\r]/g, '');
- this.buildAndSendMessage(base64Image);
-}, (err) => {
- this.buildAndSendMessage("camera not found");
-   this.currentMessage = "";
-});
+      // imageData is either a base64 encoded string or a file URI
+      // If it's base64:
+      let base64Image = 'data:image/jpeg;base64,' + imageData.replace(/[\n\r]/g, '');
+      this.buildAndSendMessage(base64Image);
+    }, (err) => {
+      this.buildAndSendMessage("camera not found");
+      this.currentMessage = "";
+    });
   }
 
 
