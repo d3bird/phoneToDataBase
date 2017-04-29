f445e63d7ffe59149979dbf16ccf096a844decf2
Scott Crawford <dogbird9@gmail.com>
2017-04-29 10:32:44 -0400

    the chats now highlight other peoples messages

--- src/providers/message-service.ts
+++ src/providers/message-service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 import { Message } from '../models/message'
-import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 /*
